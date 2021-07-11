16a38251c5aee0d8c2c5049ccd2f3d9624b09aa9
ShadowNastias <abigalekliff@gmail.com>
2021-07-11 22:20:55 +0300

    Initial commit

---
+++ ConsoleProjectByHeadFirst/Loops.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace ConsoleProjectByHeadFirst
+{
+    public class Loops
+    {
+        public string tryAnIf = TryIf();
+        public string tryAnIfElse = TryIfElse();
+        public string trySomeLoops = TryWhileFor();
+
+        public Loops(string aTryAnIf, string aTryAnIfElse, string aTrySomeLoops)
+        {
+            tryAnIf = aTryAnIf;
+            tryAnIfElse = aTryAnIfElse;
+            trySomeLoops = aTrySomeLoops;
+        }
+        
+        public static string TryIf()
+        {
+            int someValue = 4;
+            string name = "Bobbo Jr.";
+            string result;
+
+            if ((someValue == 3) && (name == "Joe"))
+            {
+               Console.WriteLine("x is 3 and the name is Joe");
+               result = Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("this line runs no matter what"); 
+                result = Console.ReadLine();
+            }
+
+            return result;
+        }
+
+        public static string TryIfElse()
+        {
+            int x = 5;
+            string result;
+            
+            if (x == 10)
+            {
+                 Console.WriteLine("x must be 10");
+                 result = Console.ReadLine();
+            }
+            else
+            {
+                 Console.WriteLine("x isn't 10");
+                 result = Console.ReadLine();
+            }
+
+            return result;
+        }
+        public static string TryWhileFor()
+        {
+            int count = 0;
+            string result;
+            
+            while (count < 10) 
+            {
+                count = count + 1;
+            }
+            for (int i = 0; i < 5; i++)
+            { 
+                count = count - 1;
+            }
+
+            Console.WriteLine("The answer is " + count);
+            result = Console.ReadLine();
+            return result;
+        }
+    }
+}
---
+++ ConsoleProjectByHeadFirst/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleProjectByHeadFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OperatorExamples();
+            Console.WriteLine( );
+            
+            //Loops
+            Console.WriteLine(Loops.TryIf());
+            Console.WriteLine(Loops.TryIfElse());
+            Console.WriteLine(Loops.TryWhileFor());
+        }
+
+        private static void OperatorExamples()
+        {
+            int width = 3;
+            width++;
+            int heigh = 2 + 4;
+            int area = width * heigh;
+
+            string result = "The area";
+            result = result + " is " + area;
+
+            bool truthValue = true;
+
+            do
+            {
+                width--;
+                Console.WriteLine("width = " + width);
+                Console.ReadLine();
+                
+                area = width * heigh;
+                Console.WriteLine("area = " + area);
+                Console.ReadLine();
+            } while (area > 25);
+        }
+    }
+}
